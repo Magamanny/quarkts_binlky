45b948b094a41f83265607c97cd61bf4fe838acb
Ali Harman <ali.harman.ansari@outlook.com>
2022-01-25 16:32:38 +0500

    debuging check with stlink on stm32f405rg, need to move the env to first entry to work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include "QuarkTS.h"
 #include <Arduino.h>
 #define TIMER_TICK 0.001 // 1ms tick
+
+// task handler
+qTask_t task1, loopTask;
+//
+void task1_Callback(qEvent_t e)
+{
+  Serial5.println("I am Back.");
+}
 void setup() {
   Serial5.begin(230400);
   pinMode(PA10,OUTPUT);
   pinMode(PC8,OUTPUT);
+  digitalWrite(PA10,HIGH);
   Serial5.println("Hello PlatformIO, Serial5");
-  qOS_Setup((qGetTickFcn_t)millis, TIMER_TICK, (qTaskFcn_t)loop);
+  qOS_Setup((qGetTickFcn_t)millis, TIMER_TICK, NULL);
+  // add task
+  qOS_Add_Task(&task1,task1_Callback,qLowest_Priority,0.1,qPeriodic,qEnabled,NULL);
+  qOS_Add_Task(&loopTask,(qTaskFcn_t)loop,qLowest_Priority,0.5,qPeriodic,qEnabled,NULL);
   // put your setup code here, to run once:
+  qOS_Run();
 }
-
+// idle task
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(PA10,HIGH);
-  digitalWrite(PC8,HIGH);
-  delay(500);
-  digitalWrite(PA10,LOW);
-  digitalWrite(PC8,LOW);
-  delay(500);
+  // this does not run
+  // arduino way of toggling
+  digitalWrite(PA10,!digitalRead(PA10));
+  digitalWrite(PC8,!digitalRead(PC8));
 }
